441714820065bd855e0687d7f7f31e8a92f9b8aa
Rakib Mondal <rakibmondal2155@gmail.com>
2026-03-05 18:43:08 +0530

    Added routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-import Product from "./components/Product";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-const App = () => <Product />;
+import PageNotFound from "./components/PageNotFound";
+import Product from "./components/Product";
+import ProductList from "./components/ProductList";
+
+const App = () => (
+  <Switch>
+    <Route exact component={ProductList} path="/products" />
+    <Route exact component={Product} path="/products/:slug" />
+    <Redirect exact from="/" to="/products" />
+    <Route component={PageNotFound} path="*" />
+  </Switch>
+);
 
 export default App;
