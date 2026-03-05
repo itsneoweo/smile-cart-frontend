659e12936eb0fc0940f963d83e218920b031b69b
Rakib Mondal <rakibmondal2155@gmail.com>
2026-03-05 16:26:04 +0530

    Integrated API to display product details

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import classNames from "classnames";
 import { Left, Right } from "neetoicons";
@@ -6,14 +6,26 @@ import { Button } from "neetoui";
 
 const Carousel = ({ imageUrls, title }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const timerRef = useRef(null);
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = setInterval(handleNext, 3000);
+  };
+
+  const handlePrevious = () => {
+    setCurrentIndex(prevIndex => (prevIndex - 1) % imageUrls.length);
+    resetTimer();
+  };
+
+  const handleNext = () => {
+    setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
+  };
 
   useEffect(() => {
-    const handleNext = () => {
-      setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
-    };
-    const interval = setInterval(handleNext, 3000);
+    timerRef.current = setInterval(handleNext, 3000);
 
-    return () => clearInterval(interval);
+    return () => clearInterval(timerRef.current);
   }, []);
 
   return (
@@ -23,9 +35,7 @@ const Carousel = ({ imageUrls, title }) => {
           className="shrink-0 focus-within:ring-0 hover:bg-transparent"
           icon={Left}
           style="text"
-          onClick={() =>
-            setCurrentIndex(prevIndex => (prevIndex - 1) % imageUrls.length)
-          }
+          onClick={handlePrevious}
         />
         <img
           alt={title}
@@ -36,9 +46,10 @@ const Carousel = ({ imageUrls, title }) => {
           className="shrink-0 focus-within:ring-0 hover:bg-transparent"
           icon={Right}
           style="text"
-          onClick={() =>
-            setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length)
-          }
+          onClick={() => {
+            handleNext();
+            resetTimer();
+          }}
         />
       </div>
       <div className="flex space-x-1">
@@ -49,7 +60,10 @@ const Carousel = ({ imageUrls, title }) => {
               "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
               { "neeto-ui-bg-black": index === currentIndex }
             )}
-            onClick={() => setCurrentIndex(index)}
+            onClick={() => {
+              setCurrentIndex(index);
+              resetTimer();
+            }}
           />
         ))}
       </div>
