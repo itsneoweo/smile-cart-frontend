8ac01117a05e2724b11a47f310364b0c335e8ea0
Rakib Mondal <rakibmondal2155@gmail.com>
2026-03-06 15:06:50 +0530

    Added add to cart feature on the product details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, React } from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
 import routes from "routes";
@@ -6,14 +6,21 @@ import routes from "routes";
 import PageNotFound from "./components/commons/PageNotFound";
 import Product from "./components/Product";
 import ProductList from "./components/ProductList";
+import CartItemsContext from "./contexts/CartItemsContext";
 
-const App = () => (
-  <Switch>
-    <Route exact component={ProductList} path={routes.products.index} />
-    <Route exact component={Product} path={routes.products.show} />
-    <Redirect exact from="/" to="/products" />
-    <Route component={PageNotFound} path="*" />
-  </Switch>
-);
+const App = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  return (
+    <CartItemsContext.Provider value={[cartItems, setCartItems]}>
+      <Switch>
+        <Route exact component={ProductList} path={routes.products.index} />
+        <Route exact component={Product} path={routes.products.show} />
+        <Redirect exact from="/" to="/products" />
+        <Route component={PageNotFound} path="*" />
+      </Switch>
+    </CartItemsContext.Provider>
+  );
+};
 
 export default App;
--- src/components/commons/index.js
+++ src/components/commons/index.js
@@ -1,9 +1,4 @@
-// export { default as Header } from "./Header";
-// export { default as PageLoader } from "./PageLoader";
-// export { default as PageNotFound } from "./PageNotFound";
-
-import Header from "./Header";
-import PageLoader from "./PageLoader";
-import PageNotFound from "./PageNotFound";
-
-export { Header, PageLoader, PageNotFound };
+export { default as Header } from "./Header";
+export { default as PageLoader } from "./PageLoader";
+export { default as PageNotFound } from "./PageNotFound";
+export { default as AddToCart } from "./AddToCart";
