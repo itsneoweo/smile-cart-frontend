e061ab9ebd5349b179eb17f2fbdfc1a63e6b3901
Rakib Mondal <rakibmondal2155@gmail.com>
2026-03-06 16:06:59 +0530

    Replaced CartItemsContext with useCartItemsStore

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, React } from "react";
+import { React } from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
 import routes from "routes";
@@ -6,21 +6,14 @@ import routes from "routes";
 import PageNotFound from "./components/commons/PageNotFound";
 import Product from "./components/Product";
 import ProductList from "./components/ProductList";
-import CartItemsContext from "./contexts/CartItemsContext";
 
-const App = () => {
-  const [cartItems, setCartItems] = useState([]);
-
-  return (
-    <CartItemsContext.Provider value={[cartItems, setCartItems]}>
-      <Switch>
-        <Route exact component={ProductList} path={routes.products.index} />
-        <Route exact component={Product} path={routes.products.show} />
-        <Redirect exact from="/" to="/products" />
-        <Route component={PageNotFound} path="*" />
-      </Switch>
-    </CartItemsContext.Provider>
-  );
-};
+const App = () => (
+  <Switch>
+    <Route exact component={ProductList} path={routes.products.index} />
+    <Route exact component={Product} path={routes.products.show} />
+    <Redirect exact from="/" to="/products" />
+    <Route component={PageNotFound} path="*" />
+  </Switch>
+);
 
 export default App;
